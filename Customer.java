59bca82688e464507ccf41cb6d86ff5a8f4a348f
too-lazy-for-necessities <65829720+too-lazy-for-necessities@users.noreply.github.com>
2020-10-19 15:09:04 +0200

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,19 @@
+package be.intecbrussel.MiniautoverkoopzaakVroem.model;
+
+public class Customer {
+
+    private int customerNumber;
+    private char customerName;
+    private char contactLastName;
+    private char contactFirstName;
+    private char phone;
+    private char addressLine1;
+    private char addressLine2;
+    private char city;
+    private char state;
+    private char postalCode;
+    private char country;
+    private int salesRepEmployeeNumber;
+    private double creditLimit;
+
+}
---
+++ Payment.java
@@ -0,0 +1,12 @@
+package be.intecbrussel.MiniautoverkoopzaakVroem.model;
+
+import java.time.LocalDate;
+
+public class Payment {
+
+    private int customerNumber;
+    private int checkNumber;
+    private LocalDate paymentDate;
+    private int amount;
+
+}
